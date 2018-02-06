d21dc958f5db37d21e03220bef79386830c712fd
Yoan Lecoq <yoanlecoq.io@gmail.com>
2018-02-06 17:32:28 +0100

    .

---
+++ vertex_group_toast.py
@@ -0,0 +1,10 @@
+D.scenes['Scene'].objects['Sphere'].vertex_groups[0]
+
+# keyframe_insert(data_path="dpath", frame=1) ??
+
+# obj.location = 3.0, 4.0, 10.0
+# obj.keyframe_insert(data_path="location", frame=1)
+
+# items()
+# get()
+# is_property_set()
