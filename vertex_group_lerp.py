4061fe2dad740488de16146ca6c7ac70fcb7df96
Yoan Lecoq <yoanlecoq.io@gmail.com>
2018-03-08 22:46:29 +0100

    Progress, needs to get correct edit-mode vertex positions

--- vertex_group_lerp.py
+++ vertex_group_lerp.py
@@ -27,7 +27,33 @@ bl_info = {
 # - Add
 # - Refresh (vertexgroup lists)
 
+
+# NOTE: vertex group code
+# scene = bpy.data.scenes['Scene']
+# obj = scene.objects['Sphere']
+# vgroup = obj.vertex_groups['TipOfTheHat']
+# vertices = [ v for v in obj.data.vertices if vgroup.index in [ vg.group for vg in v.groups ] ]
+# vpositions_model = [ v.co for v in vertices ]
+# vpositions_world = [ (obj.matrix_world * pos) for pos in vpositions_model ]
+#
+# obj['MyProp']
+# Vector.lerp()
+
+
+# NOTES:
+# - Only for Edit Mode
+
+
+# TODO:
+# - On slider change, lerp vertex positions.
+#   OR define an operator (button) that refreshes based on slider states.
+#   OR do it every frame.
+# - Have 2 modes:
+#   Edit: doesn't apply sliders
+#   Apply: does apply slider continuously. Everything's hidden except sliders.
+
 import bpy
+import bmesh
 from bpy.props import (StringProperty,
                        BoolProperty,
                        IntProperty,
@@ -43,20 +69,51 @@ from bpy.types import (Panel,
 from mathutils import Vector
 
 
+def get_vertices_of_vertex_group(obj, vgroup_name):
+    vgroup = obj.vertex_groups[vgroup_name]
+    if obj.mode == 'EDIT':
+        bm = bmesh.from_edit_mesh(obj.data)
+        out = [ v for dv, v in zip(obj.data.vertices, bm.verts) if vgroup.index in [ vg.group for vg in dv.groups ] ]
+        # bm.free() # FIXME
+        return out
+    
+    return [ v for v in obj.data.vertices if vgroup.index in [ vg.group for vg in v.groups ] ]
+
+def get_vertex_positions_modelspace(vertices):
+    return [ v.co for v in vertices ]
+
+def get_vertex_positions_worldspace(obj, vertices):
+    return [ (obj.matrix_world * pos) for pos in [ v.co for v in vertices ]]
+
+def on_slider_change(feat, context):
+    print("A change occured!", feat.name, ": ", feat.current_lerp_value)
+    # FIXME: Perform actual LERP here
+    obj = context.object
+    vgroup = obj.vertex_groups[feat.vertex_group_name]
+    i = 0
+    for v in obj.data.vertices:
+        if vgroup.index in [ vg.group for vg in v.groups ]:
+            p = feat.vpositions[0][i], feat.vpositions[1][i]
+            print("Lerp from ", p[0], " to ", p[1])
+            v.co = Vector.lerp(p[0], p[1], feat.current_lerp_value)
+            i += 1
+
 class SpecieFeature(PropertyGroup):
-    vertex_group_name = StringProperty(
+    vertex_group_name = EnumProperty(
         name = "Vertex Group Name",
         description = "Name of the vertex group to use for LERPing",
-        default = "",
-        maxlen = 128,
+        items = lambda scene, context: [(name, name, "") for name, vg in context.object.vertex_groups.items()]
         )
     current_lerp_value = FloatProperty(
         name = "Current LERP value",
         description = "A float property",
         default = 0,
         min = 0,
-        max = 1
+        max = 1,
+        update = on_slider_change
         )
+    vpositions = [[], []]
+
 
 class OBJECT_OT_specie_record_feature(bpy.types.Operator):
     bl_idname = "object.specie_record_feature"
@@ -66,6 +123,13 @@ class OBJECT_OT_specie_record_feature(bpy.types.Operator):
 
     def execute(self, context):
         print("Extremum state:", self.feature_name, self.bound)
+        obj = context.object
+        feature = obj.specie_features[self.feature_name]
+        vertices = get_vertices_of_vertex_group(obj, feature.vertex_group_name);
+        vpositions = [ v.co.copy() for v in vertices ]
+        for p in vpositions:
+            print("New vposition: ", p)
+        feature.vpositions[int(self.bound)] = vpositions
         return {'FINISHED'}
 
 class OBJECT_OT_specie_add_feature(bpy.types.Operator):
